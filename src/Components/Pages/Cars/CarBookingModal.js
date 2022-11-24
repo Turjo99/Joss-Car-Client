7ed186b6b896a9eb57c9b8081cd77e7fb7654cd0
Turjo99 <imturjo27@gmail.com>
2022-11-24 14:43:12 +0600

    modal data posted to db

--- src/Components/Pages/Cars/CarBookingModal.js
+++ src/Components/Pages/Cars/CarBookingModal.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import toast from "react-hot-toast";
 import { AuthContext } from "../../../Context/UserContext";
 
 const CarBookingModal = ({ carDetail }) => {
@@ -7,6 +8,35 @@ const CarBookingModal = ({ carDetail }) => {
   const handleBooking = (event) => {
     event.preventDefault();
     const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const price = form.price.value;
+    const phone = form.phone.value;
+    const location = form.location.value;
+    const booking = {
+      product: carName,
+      name: name,
+      email: email,
+      price: price,
+      phone: phone,
+      location: location,
+    };
+    fetch("http://localhost:5000/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          toast.success("Booking confirmed");
+        } else {
+          toast.error(data.message);
+        }
+      });
   };
 
   return (
@@ -44,6 +74,7 @@ const CarBookingModal = ({ carDetail }) => {
             <input
               type="text"
               disabled
+              name="price"
               value={sellPrice}
               className="input w-full input-bordered "
             />
