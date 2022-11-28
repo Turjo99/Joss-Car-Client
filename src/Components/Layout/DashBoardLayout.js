a562b3167c9bf3f35fc9a8d46d09d29bd966250e
Turjo99 <imturjo27@gmail.com>
2022-11-28 22:44:48 +0600

    drawer bg cleared

--- src/Components/Layout/DashBoardLayout.js
+++ src/Components/Layout/DashBoardLayout.js
@@ -25,7 +25,7 @@ const DashBoardLayout = () => {
         </div>
         <div className="drawer-side">
           <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+          <ul className="menu p-4 w-80 ">
             {isAdmin && (
               <>
                 <li className=" bg-slate-800 my-5 text-white">
